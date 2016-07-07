c2af048791bf02450dfc8f5b92933b8913278c65
riva-spanos <riva.spanos@gmail.com>
2016-07-06 21:19:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 var main = function() {
-  $('.dropdown-toggle').click(function() {
-    $('.dropdown-menu').toggle();
-  });
 
-  
   $('.arrow-next').click(function() {
     var currentSlide = $('.active-slide');
     var nextSlide = currentSlide.next();
